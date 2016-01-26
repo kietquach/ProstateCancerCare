d53dac73d334babd96aa86448e6c72971e362f01
Kiet Quach <kvquach@ucdavis.edu>
2016-01-26 01:34:17 -0800

    Added Parse
    Added Parse for now. Still need to create Parse account, add app id and
    client key to android studio.

---
+++ app/src/main/java/app/com/example/ecs193/prostatecancercare/MainActivity.java
@@ -0,0 +1,10 @@
+package app.com.example.ecs193.prostatecancercare;
+
+import android.app.Activity;
+
+/**
+ * Created by Kiet Quach on 1/26/2016.
+ */
+public class MainActivity extends Activity{
+
+}
