da9adb4da71e8ccc17e29fe7e423b6fb30eb3ad8
XiaolinLi <xili7@ucdavis.edu>
2016-01-29 21:51:40 -0800

    show alert dialog

--- app/src/main/java/app/com/example/ecs193/prostatecancercare/ClientFirstTime.java
+++ app/src/main/java/app/com/example/ecs193/prostatecancercare/ClientFirstTime.java
@@ -1,7 +1,10 @@
 package app.com.example.ecs193.prostatecancercare;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ClientFirstTime extends Activity {
 
@@ -11,5 +14,24 @@ public class ClientFirstTime extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_first_time);
+    }
+
+    //the onclick function for the submit button in activity_client_first_time.xml.
+    public void onSubmit(View v) {
+        switch(v.getId()) {
+            case R.id.Button_submit:
+                EditText ageEditView = (EditText) findViewById(R.id.EditText_age);
+                if(ageEditView.getText() == null) {
+                    return;
+                }
+
+                Double age = Double.parseDouble(ageEditView.getText().toString());
+                if(age > 20) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("Abnormal PSA value. Please go see a doctor.");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+        }
     }
 }
