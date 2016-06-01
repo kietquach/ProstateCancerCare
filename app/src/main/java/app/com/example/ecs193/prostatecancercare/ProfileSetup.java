79210acf44218423f9a0646d4ee751f91a0dcf3f
XiaolinLi <xili7@ucdavis.edu>
2016-06-01 00:15:41 -0700

    Retrieve profile info, change text color

--- app/src/main/java/app/com/example/ecs193/prostatecancercare/ProfileSetup.java
+++ app/src/main/java/app/com/example/ecs193/prostatecancercare/ProfileSetup.java
@@ -11,8 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 import java.util.Map;
 
@@ -26,22 +28,38 @@ public class ProfileSetup extends AppCompatActivity {
         Firebase.setAndroidContext(this);
 
         fbRef = new Firebase("https://boiling-heat-3817.firebaseio.com/");
+        AuthData authData = fbRef.getAuth();
+        final Firebase profile = fbRef.child("users").child(authData.getUid()).child("profile");
+        profile.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    ((EditText) findViewById(R.id.firstnameEdit)).setText(dataSnapshot.child("firstname").getValue().toString());
+                    ((EditText) findViewById(R.id.lastnameEdit)).setText(dataSnapshot.child("lastname").getValue().toString());
+                    ((EditText) findViewById(R.id.dobEdit)).setText(dataSnapshot.child("dob").getValue().toString());
+                    ((EditText) findViewById(R.id.ethnicityEdit)).setText(dataSnapshot.child("ethnicity").getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
 
         Button createButton = (Button) findViewById(R.id.createButton);
         createButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                                        AuthData authData = fbRef.getAuth();
-                                                                Firebase userRef = fbRef.child("users").child(authData.getUid());
                                         String dob = ((EditText) findViewById(R.id.dobEdit)).getText().toString();
                                         String firstname = ((EditText) findViewById(R.id.firstnameEdit)).getText().toString();
                                         String lastname = ((EditText) findViewById(R.id.lastnameEdit)).getText().toString();
                                         String ethnicity = ((EditText) findViewById(R.id.ethnicityEdit)).getText().toString();
                                         //TODO: Fix DOB, month day year all should be one string (ex. 01012016)
 
-
-                                        Firebase profile = userRef.child("profile");
                                         profile.child("firstname").setValue(firstname);
                                         profile.child("lastname").setValue(lastname);
                                         profile.child("dob").setValue(dob);
